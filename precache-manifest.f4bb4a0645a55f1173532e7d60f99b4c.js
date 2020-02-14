665f705c1ddf63993760067b8444b61afa7d4a90
unknown <trujillorodriguezjavier@gmail.com>
2020-02-14 17:58:44 +0100

    Updates

---
+++ precache-manifest.f4bb4a0645a55f1173532e7d60f99b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628b999233a8b1ef028d97f6ff635a1c",
+    "url": "/cocktail_world/index.html"
+  },
+  {
+    "revision": "6473ac258cf97699f672",
+    "url": "/cocktail_world/static/css/main.19b1cede.chunk.css"
+  },
+  {
+    "revision": "53225b5bf54bdd35183e",
+    "url": "/cocktail_world/static/js/2.483f0470.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cocktail_world/static/js/2.483f0470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6473ac258cf97699f672",
+    "url": "/cocktail_world/static/js/main.7e216bf5.chunk.js"
+  },
+  {
+    "revision": "d3499c8265cfcf916cca",
+    "url": "/cocktail_world/static/js/runtime-main.9783858d.js"
+  },
+  {
+    "revision": "a1481eeb043d4132bf0c2548acac21bf",
+    "url": "/cocktail_world/static/media/logo.a1481eeb.png"
+  }
+]);
